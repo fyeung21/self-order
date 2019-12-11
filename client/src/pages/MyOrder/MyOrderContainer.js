991850b15076e9b849a9a069b6f18c7d47adb199
fyeung21 <fyeung21@gmail.com>
2019-12-10 19:37:52 -0800

    sketch basic pages

---
+++ client/src/pages/MyOrder/MyOrderContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import MyOrder from './MyOrder';
+
+class MyOrderContainer extends Component {
+    render() {
+        return <MyOrder />;
+    }
+}
+export default MyOrderContainer;
---
+++ client/src/pages/SingleItem/SingleItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SingleItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>this is one single item page found at /single-item/id:1</p>
+            </div>
+        );
+    }
+}
+export default SingleItem;
