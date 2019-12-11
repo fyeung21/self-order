706cf9075d6e9d3cd88397a9b036e6935c3b44f3
rickychan0611 <ric0611@gmail.com>
2019-12-10 18:52:33 -0800

    sementicUi installed

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
+
 import App from '/imports/ui/App'
 
 Meteor.startup(() => {
