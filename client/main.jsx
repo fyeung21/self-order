3efb8d10e23f983d518a7fa3abfcab95fae3fe47
rickychan0611 <ric0611@gmail.com>
2019-12-11 17:11:40 -0800

    installed semantic-ui-react

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,6 @@
 // import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
 import { renderRoutes } from './src/routes';
 
 
