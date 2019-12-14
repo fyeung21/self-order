f8989654963c8141400d64865343ce03a706a281
rickychan0611 <ric0611@gmail.com>
2019-12-13 17:49:58 -0800

    attempted to make the menu page with grid

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,19 @@
 // import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import React, { Fragment, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import { renderRoutes } from './src/routes';
+import { Container, Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
+const App = () => {
+  return(
+    <Container style={{ margin: 20 }}>
+        {renderRoutes()}
+    </Container>
+  )
+}
 
 Meteor.startup(() => {
-  render(renderRoutes(), document.getElementById('react-target'));
+  ReactDOM.render(<App />, document.getElementById('react-target'));
 });
--- client/src/pages/Menu/MenuContainer.js
+++ client/src/pages/Menu/MenuContainer.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import Menu from './Menu';
+import Item from './Item';
+import { Grid } from 'semantic-ui-react'
 
-class MenuContainer extends Component {
-    render() {
-        return <Menu />;
-    }
+const menuItems = require('./menu.json');
+
+const MenuContainer = () => {
+    return (
+    // <Grid doubling centered columns={8} verticalAlign>
+        <Grid centered>
+            <Grid.Row>
+            {menuItems.map(item => {
+                return <Item item={item}/>
+            })}
+            </Grid.Row>
+    </Grid>
+    )
 }
 export default MenuContainer;
