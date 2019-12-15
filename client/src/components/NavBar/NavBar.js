78ee4f76f516dd65a2ddb2f2f964dfd913233a7d
fyeung21 <fyeung21@gmail.com>
2019-12-14 16:04:14 -0800

    add navBar and style with icons

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import "./NavStyle";
+
+
+const NavBar = () => {
+    // state = {}
+    // handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    // render() {
+    //     const { activeItem } = this.state
+
+    return (
+        <Menu fluid widths={3}>
+
+            <Menu.Item
+                name='Menu'
+            // active={activeItem === 'buy'}
+            // onClick={this.handleItemClick}
+            >
+                <div className="menu-container">
+                    <Icon name='food' size='big' />
+                    <span>Menu</span>
+                </div>
+            </Menu.Item>
+
+            <Menu.Item
+                name='Service'
+            // active={activeItem === 'sell'}
+            // onClick={this.handleItemClick}
+            >
+                <div className="menu-container">
+                    <Icon name='bell outline' size='big' />
+                    <span>Service</span>
+                </div>
+            </Menu.Item>
+
+            <Menu.Item
+                name='My Order'
+            // active={activeItem === 'rent'}
+            // onClick={this.handleItemClick}
+            >
+                <div className="menu-container">
+                    <Icon name='edit outline' size='big' />
+                    <span>My Order</span>
+                </div>
+            </Menu.Item>
+
+        </Menu >
+    )
+    // }
+}
+export default NavBar;
--- client/src/pages/MyOrder/MyOrder.js
+++ client/src/pages/MyOrder/MyOrder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from "../../components/NavBar";
 
 class MyOrder extends Component {
     render() {
         return (
             <div>
                 <p>this is the My Order page found at /my-order</p>
+                <NavBar />
             </div>
         );
     }
