1ff9d8f329a939e8815446deef365c198029dcfb
fyeung21 <fyeung21@gmail.com>
2019-12-16 16:30:46 -0800

    add time and funct layout for kitchen

--- client/src/pages/Kitchen/Kitchen.js
+++ client/src/pages/Kitchen/Kitchen.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import KitchenCard from './../../components/KitchenCard';
+import { Button } from 'semantic-ui-react';
+import "./styles";
+import tableNumber from '../../components/tableNumber';
 
 const Kitchen = () => {
+    // const date = new Date();
+
     return (
-        <div>
+        <div className="kitchenBackground">
             <p>This is what the kitchen would see.</p>
-            <KitchenCard />
+            <h1>{"01/23/2020"}</h1>
+            <div>
+                <h3>{"time created 14:20"}</h3>
+                <tableNumber />
+                <div>
+                    <KitchenCard />
+                    <KitchenCard />
+                    <KitchenCard />
+                </div>
+                <h3>{"time done 14:40 (will show time when 'done' is pressed)"}</h3>
+                <Button basic color="blue">Done</Button>
+            </div>
         </div>
     )
 }
