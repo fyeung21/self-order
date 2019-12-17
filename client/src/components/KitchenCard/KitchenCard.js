351f08540793f671161a6a8bcd24159bfac6975e
fyeung21 <fyeung21@gmail.com>
2019-12-16 16:02:44 -0800

    sketch kitchenCard

--- client/src/components/KitchenCard/KitchenCard.js
+++ client/src/components/KitchenCard/KitchenCard.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Grid, Image, Card, Modal } from 'semantic-ui-react';
+import { Grid, Image, Card, Modal, Button } from 'semantic-ui-react';
 import "./kitCardStyle.css";
 
 const KitchenCard = ({ item }) => {
 
     return (
         <div>
-            <p>box</p>
+            <Card>
+                <Card.Content>
+                    <Image
+                        floated='right'
+                        size='mini'
+                        src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                    />
+                    <Card.Header>{"Shrimp Dumplings"}</Card.Header>
+                    <Card.Meta>Qty: {"3"}</Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color='green'>
+                            Ready/Out
+                        </Button>
+                    </div>
+                </Card.Content>
+            </Card>
         </div>
     )
 }
