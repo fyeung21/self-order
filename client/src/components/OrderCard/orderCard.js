4474aca7cc59bd8dddc128a3d74438c6ac99109f
jose <jmvr37@hotmail.com>
2019-12-17 19:05:52 -0800

    style my order card

--- client/src/components/OrderCard/orderCard.js
+++ client/src/components/OrderCard/orderCard.js
@@ -15,40 +15,39 @@ import "./styles.css";
 const OrderCard = () => {
   return (
     <Card.Group>
-      <Card>
+      <Card className="card1">
         <Card.Content>
-          <Image
-            floated="right"
-            size="mini"
-            src="http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-shrimp-dumplings-thumb.jpg"
-          />
-          <Card.Header>Dim-Sum bbq</Card.Header>
-          <Card.Meta>3 pieces</Card.Meta>
-        </Card.Content>
-        <Card.Content extra>
-          <div className="ui two buttons">
-            <Button basic color="red">
-              <Icon name="trash alternate outline" size="large" />
-            </Button>
+          <div className="content1">
+            <Image
+              className="image-card"
+              // floated="right"
+              // size="small"
+              src="http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-shrimp-dumplings-thumb.jpg"
+            />
+            <Card.Header>
+              Dim-Sum bbq
+              <Card.Meta>3 pcs</Card.Meta>
+            </Card.Header>
+            <div>
+              <Icon name="trash alternate outline" size="large" color="red" />
+            </div>
           </div>
         </Card.Content>
       </Card>
-      <Card>
+      <Card className="card1">
         <Card.Content>
-          <Image
-            className="img"
-            floated="right"
-            size="mini"
-            src="http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-pork-dumplings-thumb.jpg"
-          />
-          <Card.Header>Egg Custard</Card.Header>
-          <Card.Meta>4 pieces</Card.Meta>
-        </Card.Content>
-        <Card.Content extra>
-          <div className="ui two buttons">
-            <Button basic color="red">
-              <Icon name="trash alternate outline" size="large" />
-            </Button>
+          <div className="content1">
+            <Image
+              className="image-card"
+              src="http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-pork-dumplings-thumb.jpg"
+            />
+            <Card.Header>
+              Egg Custard
+              <Card.Meta>4 pcs</Card.Meta>
+            </Card.Header>
+            <div>
+              <Icon name="trash alternate outline" size="large" color="red" />
+            </div>
           </div>
         </Card.Content>
       </Card>
