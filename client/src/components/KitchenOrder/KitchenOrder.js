4ec14641c410e37a66a7ad5c0680404ec79bd5c8
fyeung21 <fyeung21@gmail.com>
2019-12-17 20:39:35 -0800

    edit kitchenOrder positioning

--- client/src/components/KitchenOrder/KitchenOrder.js
+++ client/src/components/KitchenOrder/KitchenOrder.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Grid, Image, Card, Modal, Button } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import TableNumber from '../../components/tableNumber';
 import KitOrderCard from './KitOrderCard/KitOrderCard';
 import "./kitchenStyles.css";
 
-const KitchenOrder = ({ item }) => {
+const KitchenOrder = () => {
 
     return (
         <div>
-            <h3>{"time created 14:20"}</h3>
-            <TableNumber />
-            <div>
+            <div className="header">
+                <h3>{"14:20"}</h3> {/* time created */}
+                <TableNumber />
+            </div>
+            <div className="kitCard">
                 <KitOrderCard />
                 <KitOrderCard />
                 <KitOrderCard />
             </div>
-            <h3>{"time done 14:40 (will show time when 'done' is pressed)"}</h3>
-            <Button basic color="blue">Done</Button>
+            <h3 textAlign="center">{"14:40"}</h3> {/* show time when 'done' is pressed */}
+            <Button size="large" basic color="white" className="doneBtn">Done</Button>
         </div>
     )
 }
