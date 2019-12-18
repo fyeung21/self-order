e0562d163a6af3cd96e6e2af68cc2400c0990e14
rickychan0611 <ric0611@gmail.com>
2019-12-17 21:15:17 -0800

    connect menu to the db and able to insert item to the db

--- server/main.js
+++ server/main.js
@@ -1,31 +1,96 @@
+import _ from 'lodash'
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
+import { Menu } from '/imports/api/collections/menu';
+import { GlobalOrders } from '/imports/api/collections/globalOrders';
 
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
+
+
+// function insertLink(title, url) {
+//   Links.insert({ title, url, createdAt: new Date() });
+// }
+const items = [
+  {
+    "name": "Shrimp Dumplings",
+    "price" : 7,
+    "pcs" : 4,
+    "description" : "Whole shrimp in a translucent wrapper.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-shrimp-dumplings-thumb.jpg",
+    "featured" : false
+},
+{
+    "name": "Steamed Soup Dumplings",
+    "price" : 10,
+    "pcs" : 8,
+    "description" : "Dumplings filled with pork, crab meat and broth.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-pork-dumplings-thumb.jpg",
+    "featured" : false
+},
+{
+    "name": "Pork Siu Mai",
+    "price" : 7,
+    "pcs" : 4,
+    "description" : "Open-topped dumplings filled with ground pork and shrimp.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-pork-siu-mai-thumb.jpg",
+    "featured" : false
+}, 
+{
+    "name": "Steamed Pork Ribs",
+    "price" : 7,
+    "pcs" : 4,
+    "description" : "Pork rib tips steamed with whole black beans and oil.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-pork-ribs-thumb.jpg",
+    "featured" : false
+},   
+{
+    "name": "Steamed Chicken Feet",
+    "price" : 7,
+    "pcs" : 4,
+    "description" : "Chewy chicken feet that are fried, marinated and steamed.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-chicken-feet-thumb.jpg",
+    "featured" : false
+},   
+{
+    "name": "Sticky Rice in Lotus Leaf",
+    "price" : 7,
+    "pcs" : 1,
+    "description" : "Pork, chicken, sausage and mushrooms wrapped in a lotus leaf.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-sticky-rice-in-lotus-leaf-thumb.jpg",
+    "featured" : false
+},     
+{
+    "name": "Steamed Pork Buns",
+    "price" : 7,
+    "pcs" : 3,
+    "description" : "BBQ pork wrapped in sweet dough and steamed.",
+    "cataglory" : " Steamed",
+    "imgurl" : "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-steamed-pork-buns-thumb.jpg",
+    "featured" : false
 }
-
+]
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
+  //add items from my menu.json when there is db is emtpy
+  // const menuItems = Menu.find({}).count()
+  //   if (!menuItems) {
+  //     //make some data
+  //     for (let i in items){
+  //       Menu.insert(
+  //         items[i]
+  //     )
+  //   }
+  // }
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
+  //publish then menu collection
+  Meteor.publish('menu', function() {
+    return Menu.find({})
+  })
 
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
+  Meteor.publish('user', function() {
+    return User.find({})
+  })
 });
