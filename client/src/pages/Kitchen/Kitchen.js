e099d0788a289f2ab5637a8cc9f6d3987b0400b2
fyeung21 <fyeung21@gmail.com>
2019-12-17 18:50:53 -0800

    add today's date with js

--- client/src/pages/Kitchen/Kitchen.js
+++ client/src/pages/Kitchen/Kitchen.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import KitchenOrder from '../../components/KitchenOrder';
+import { Grid } from 'semantic-ui-react'
 import "./styles";
 
 const Kitchen = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+
     return (
         <div className="kitchenBackground">
-            <p>This is what the kitchen would see.</p>
-            <h1>{"01/23/2020"}</h1>
-            <div>
-                <KitchenOrder />
-            </div>
+            <Grid>
+                <Grid.Column>
+                    <p>This is what the kitchen would see.</p>
+                    <h1>{today}</h1>
+                    <div>
+                        <KitchenOrder />
+                    </div>
+                </Grid.Column>
+            </Grid>
         </div>
     )
 }
