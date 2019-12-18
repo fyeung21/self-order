1c0aa60651a346f2f605273a577bcbec28c273a1
jose <jmvr37@hotmail.com>
2019-12-17 18:22:34 -0800

    remove description from my order card

--- client/src/components/OrderCard/orderCard.js
+++ client/src/components/OrderCard/orderCard.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Button, Icon, Card, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import {
+  Button,
+  Icon,
+  Card,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 import "./styles.css";
 
 const OrderCard = () => {
@@ -14,10 +24,6 @@ const OrderCard = () => {
           />
           <Card.Header>Dim-Sum bbq</Card.Header>
           <Card.Meta>3 pieces</Card.Meta>
-          <Card.Description>
-            Beef Balls. Seasoned ground or minced beef, placed on bean curd
-            skins and steamed
-          </Card.Description>
         </Card.Content>
         <Card.Content extra>
           <div className="ui two buttons">
@@ -37,9 +43,6 @@ const OrderCard = () => {
           />
           <Card.Header>Egg Custard</Card.Header>
           <Card.Meta>4 pieces</Card.Meta>
-          <Card.Description>
-            A steamed custard that may include meat or seafood.
-          </Card.Description>
         </Card.Content>
         <Card.Content extra>
           <div className="ui two buttons">
