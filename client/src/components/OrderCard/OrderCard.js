3bab971067ab19fea780e54b1d0dd414c3508430
jose <jmvr37@hotmail.com>
2020-01-02 19:56:31 -0800

    my order page changes

--- client/src/components/OrderCard/OrderCard.js
+++ client/src/components/OrderCard/OrderCard.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from 'react';
+import React, { Component, Fragment, useState } from "react";
 import {
   Button,
   Icon,
@@ -9,30 +9,33 @@ import {
   Image,
   Message,
   Segment
-} from 'semantic-ui-react';
-import './styles.css';
+} from "semantic-ui-react";
+import "./styles.css";
+
 const OrderCard = ({ item }) => {
   state = {
     items: [
       {
-        name: '1111Shrimp Dumplings',
+        name: "1111Shrimp Dumplings",
         price: 7,
         pcs: 4,
-        description: 'Whole shrimp in a translucent wrapper.',
-        cataglory: ' Steamed',
+        description: "Whole shrimp in a translucent wrapper.",
+        cataglory: " Steamed",
         imgurl:
-          'http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-shrimp-dumplings-thumb.jpg',
+          "http://www.dimsumcentral.com/wp-content/uploads/2016/01/steamed-shrimp-dumplings-thumb.jpg",
         featured: false
       }
     ]
   };
+
   handleDelete = itemId => {
     // const items = this.state.items.filter(item => item.id !== itemId);
     // this.setState({ items: items });
-    alert('Button clicked');
+    alert("Button clicked");
   };
   const [qty, setQty] = useState(0);
   const [visible, setVisible] = useState(true);
+
   const handlePlusQty = () => {
     let counter = qty;
     counter++;
@@ -40,42 +43,44 @@ const OrderCard = ({ item }) => {
   };
   const handleMinusQty = () => {
     let counter = qty;
+
     if (counter > 0) {
       counter--;
       setQty(counter);
     }
   };
+
   // const OrderCard = () => {
   return (
-    <Card fluid className='card1'>
+    <Card fluid className="card1">
       <Card.Content>
-        <div className='content1'>
+        <div className="content1">
           <Image
-            className='image-card'
-            // floated='right'
-            // size='small'
+            className="image-card"
+            // floated="right"
+            // size="small"
             src={item.imgurl}
           />
-          <Card.Header className='header-my-order'>
+          <Card.Header className="header-my-order">
             {item.name}
             <Card.Header>3 pcs</Card.Header>
             <Card.Meta>Qty:{qty}</Card.Meta>
-            <div className='qty2'>
+            <div className="qty2">
               <div>
                 <Button
                   circular
-                  icon='minus'
+                  icon="minus"
                   onClick={handleMinusQty}
-                  className='add-btn'
+                  className="add-btn"
                 />
               </div>
-              <h2 className='qtyNum1'>{qty}</h2>
+              <h2 className="qtyNum1">{qty}</h2>
               <div>
                 <Button
                   circular
-                  icon='plus'
+                  icon="plus"
                   onClick={handlePlusQty}
-                  className='add-btn'
+                  className="add-btn"
                 />
               </div>
             </div>
@@ -83,12 +88,12 @@ const OrderCard = ({ item }) => {
           <div>
             <Button
               basic
-              icon='trash alternate outline'
-              size='large'
-              color='red'
+              icon="trash alternate outline"
+              size="large"
+              color="red"
               onClick={this.handleDelete}
             >
-              {/* <Icon name='trash alternate outline' size='large' color='red' /> */}
+              {/* <Icon name="trash alternate outline" size="large" color="red" /> */}
             </Button>
           </div>
         </div>
@@ -96,4 +101,5 @@ const OrderCard = ({ item }) => {
     </Card>
   );
 };
+
 export default OrderCard;
