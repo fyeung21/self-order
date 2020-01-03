0b780ff0265514815ca3dac23166ece5a86d3af6
Fiona <fyeung21@gmail.com>
2020-01-02 20:04:56 -0800

    add complete order toggle btn

--- client/src/components/KitchenOrder/KitchenOrder.js
+++ client/src/components/KitchenOrder/KitchenOrder.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import TableNumber from '../TableNumber/TableNumber';
 import KitOrderCard from './KitOrderCard/KitOrderCard';
 import "./kitchenStyles.css";
 
 const KitchenOrder = () => {
+    const [active, setActive] = useState(false)
+
+    handleClick2 = () => {
+        setActive(!active)
+    }
+
+    const CompleteBtn = () => {
+        return (
+            <div>
+                <Button
+                    className="doneBtn"
+                    basic
+                    size="large"
+                    color='grey'
+                    toggle active={active}
+                    onClick={this.handleClick2} >
+                    Complete
+                </Button>
+            </div>
+        )
+
+    }
+    const NotDoneBtn = () => {
+        return (
+            <div>
+                <Button
+                    className="doneBtn"
+                    size="large"
+                    color='yellow'
+                    toggle active={active}
+                    onClick={this.handleClick2} >
+                    Done
+                </Button>
+            </div>
+        )
+
+    }
 
     return (
         <div>
@@ -18,7 +55,7 @@ const KitchenOrder = () => {
                 <KitOrderCard />
             </div>
             <h3 textalign="center">{"14:40"}</h3> {/* show time when 'done' is pressed */}
-            <Button size="large" basic color="red" className="doneBtn">Done</Button>
+            {active ? <CompleteBtn /> : <NotDoneBtn />}
         </div>
     )
 }
